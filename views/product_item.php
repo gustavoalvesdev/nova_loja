0ee65e9499a88437475d7698224c8d637e5fd465
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2022-02-28 14:35:43 -0300

    Criando a Home (4/4)

--- views/product_item.php
+++ views/product_item.php
@@ -1,17 +1,29 @@
 <div class="product_item">
     <a href="">
         <div class="product_tags">
-            <div class="product_tag product_tag_red">SALE</div>
-            <div class="product_tag product_tag_green">BESTSELLER</div>
-            <div class="product_tag product_tag_blue">NEW!</div>
+            <?php if ($sale == '1'): ?>
+                <div class="product_tag product_tag_red"><?php $this->lang->get('SALE'); ?></div>
+            <?php endif; ?>
+            <?php if ($bestseller == '1'): ?>
+                <div class="product_tag product_tag_green"><?php $this->lang->get('BESTSELLER'); ?></div>
+            <?php endif; ?>
+            <?php if ($new_product == '1'): ?>
+            <div class="product_tag product_tag_blue"><?php $this->lang->get('NEW'); ?></div>
+            <?php endif; ?>
         </div>
         <div class="product_image">
-            <img src="<?php echo BASE_URL ?>media/products/1.jpg" width="100%" alt="">
+            <img src="<?php echo BASE_URL ?>media/products/<?php echo $images[0]['url']; ?>" width="100%" alt="">
         </div>
-        <div class="product_name">Monitor XXXXXXXX</div>
-        <div class="product_brand">SONY</div>
-        <div class="product_price_from">R$ XXXX</div>
-        <div class="product_price">R$ XXXX</div>
+        <div class="product_name"><?php echo $name; ?></div>
+        <div class="product_brand"><?php echo $brand_name; ?></div>
+        <div class="product_price_from">
+            <?php 
+                if ($price_from != '0') {
+                    echo 'R$ ' . number_format($price_from, 2, ',', '.');
+                }
+            ?>
+        </div>
+        <div class="product_price">R$ <?php echo number_format($price, 2, ',', '.'); ?></div>
         <div style="clear: both;"></div>
     </a>
 </div>
