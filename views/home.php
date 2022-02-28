66ac938046c39345bd3aa373b0ba46b86c19d7b5
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2022-02-28 13:48:16 -0300

    Criando a Home (3/4)

--- views/home.php
+++ views/home.php
@@ -1,13 +1,22 @@
 <div class="row">
+<?php
+
+    $a = 0;
+
+    foreach($list as $product_item): 
+?>
     <div class="col-sm-4">
         
-        <?php $this->loadView('product_item', array()); ?>
+        <?php $this->loadView('product_item', $product_item); ?>
 
     </div>
-    <div class="col-sm-4">
-        ...
-    </div>
-    <div class="col-sm-4">
-        ...
-    </div>
+    <?php 
+        if ($a >= 2) {
+            $a = 0;
+            echo '</div><div class="row">';
+        } else {
+            $a++;
+        }
+    ?>
+<?php endforeach; ?>
 </div>
